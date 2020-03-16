351760864568506421d36aa962d77327c48ab8db
Waleed A.K <wabdulkader@Falconinsgroup.com>
2020-03-16 18:17:05 -0500

    Add New Project

---
+++ Tools/Extensions/Ext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Tools
+{
+    public static class Ext
+    {
+        #region Public Methods
+
+        public static T With<T>(this T @this, Action<T> action)
+        {
+            action(@this);
+            return @this;
+        }
+
+        #endregion Public Methods
+    }
+}
